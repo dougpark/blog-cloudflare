6921776f233cbae7629584976c1ccaa39ab5bb3d
dougpark <parkdn@gmail.com>
2026-03-22 16:43:58 +0000

    Update from https://github.com/dougpark/64zbit.com_pelican/commit/257754ab084d97a6901302ccd61e91d0bfa6c0f5

--- projects/hello/main.js
+++ projects/hello/main.js
@@ -8,10 +8,27 @@ function createGame(width, height) {
         height: height,
         scene: {
             create: function () {
-                this.add.text(width / 2, height / 2, 'Hello Phaser!', {
+                const text = this.add.text(width / 2, height / 2, 'Hello Phaser!', {
                     font: '24px Arial',
                     fill: '#fff'
                 }).setOrigin(0.5);
+
+                // Array of colors to cycle through
+                const colors = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', '#ff00ff'];
+                let colorIndex = 0;
+
+                // Create an infinite tween that cycles through colors
+                this.tweens.add({
+                    targets: { colorValue: 0 },
+                    colorValue: 1,
+                    duration: 500,
+                    ease: 'Linear',
+                    loop: -1,
+                    onUpdate: (tween, target) => {
+                        colorIndex = Math.floor(target.colorValue * colors.length) % colors.length;
+                        text.setFill(colors[colorIndex]);
+                    }
+                });
             }
         },
         scale: {
